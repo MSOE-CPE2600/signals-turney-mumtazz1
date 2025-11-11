f9eb90642b91802b7ff1467d6c0e624b587e9d9e
Zoya Mumtaz <mumtazz@msoe.edu>
2025-11-10 22:11:56 -0600

    Part 4

--- recv_signal.c
+++ recv_signal.c
@@ -1,10 +1,40 @@
 /**
- * File: 
- * Modified by:
+ * File: recv_signal.c
+ * Modified by: Zoya Mumtaz
  * 
- * Brief summary of program:
+ * Brief summary of program: handles SIGUSR1 signal and prints sival_int
  */
 
- int main(void){
-    return 0;
+ #include <stdio.h>
+ #include <unistd.h>     
+ #include <signal.h>     
+ #include <string.h>   
+ #include <sys/types.h> 
+
+
+
+ void handle_sigusr1(int sig, siginfo_t *info, void *ucontext){
+
+    int sival_val = info->si_value.sival_int; //sival_int inside info 
+    printf("The received sival_int value is: %d\n", sival_val);
+    // do not exit
  }
+
+ int main(void){
+    struct sigaction sa; //the sigaction struct
+    //assign the handler to the struct's signal handler pointer
+    sa.sa_sigaction = handle_sigusr1; 
+    sa.sa_flags = SA_SIGINFO; //used when signal handler has 3 args
+
+   sigaction(SIGUSR1, &sa, NULL);
+
+   //print the PID to make it easier for the user to send signal
+   printf("Program has PID: %d\n", getpid());
+   
+   while(1){ //infinite loop
+    sleep(1);
+    //does not print anything
+   }
+
+   return 0;
+}
--- send_signal.c
+++ send_signal.c
@@ -1,10 +1,48 @@
 /**
- * File: 
- * Modified by:
+ * File: send_signal.c
+ * Modified by: Zoya Mumtaz
  * 
- * Brief summary of program:
+ * Brief summary of program: sends SIGUSR1 signal and a random int
+ * to another process (recv_signal.c executing)
  */
 
- int main(void){
+ #include <stdio.h>
+ #include <stdlib.h>    
+ #include <signal.h>   
+ #include <sys/types.h>
+ #include <time.h>  
+
+ int main(int argc, char* argv[]){
+
+    //get the argument with the PID
+    if (argc != 2){
+        puts("Error: Please enter the PID number");
+        exit(1);
+    }
+
+    //get the pid arg and convert
+    pid_t the_pid = (pid_t)atoi(argv[1]); 
+    //check if the pid is valid
+    if (the_pid <= 1){
+        perror("Error: ");
+        exit(1);
+    }
+
+    //generate random number
+    //seed the generator with current time
+    srand(time(NULL));
+    int random = rand();
+
+    //data to send 
+    union sigval send_val;
+    send_val.sival_int = random; //assign sival_int the random value
+    printf("Sending SIGUSR1 to PID: %d\n", the_pid);
+    printf("The number being sent is %d\n", random);
+
+    //sigqueue to send the signal and number
+    //int sigqueue(pid_t pid, int sig, const union sigval value);
+    sigqueue(the_pid, SIGUSR1, send_val);
+    puts("The signal has been sent to the specified PID.");
     return 0;
+
 }
